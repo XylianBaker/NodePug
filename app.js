a11a5701d012d48f0bf71665613133b63c5ed006
Jan Kammellander <janschwiderik@gmail.com>
2021-10-04 11:07:37 +0200

    node runs index.html

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello, World!');
+    res.status(200).sendFile(__dirname + '/index.html');
 });
 
 app.listen(port, () => {
