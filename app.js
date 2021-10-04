ea4648f87b3457df890a0ad20fc1f7075c70d1a6
Jan Kammellander <janschwiderik@gmail.com>
2021-10-04 13:24:53 +0200

    pug file gets rendered in app.js

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ const port = 3000;
 
 // http://localhost:3000
 
+// directs to the directory, where the templates are kept 🐶
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 // handles the unspecified or non-existent paths 🏗️
 app.use((req, res, next) => {
     console.log(`URL: ${req.url}`);
     next();
 });
 
-// forward to index.html
+// renders the pug file
 app.get('/', (req, res) => {
-    res.status(200).sendFile(__dirname + '/index.html');
+    res.render('index');
 });
 
 // if index.hhtml is not found 🔥
